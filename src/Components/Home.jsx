922f9f9811ea1ca6d1cd4ec15f7a4e0c67a2d147
sumit saini <sumitsaini95488@gmail.com>
2022-09-16 20:09:54 +0530

    right Frontend

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -10,7 +10,7 @@ function Home() {
       </div>
       <div className="md:w-3/5 md:py-5 md:pl-16 md:pr-8 ">
         <h1 className="mt-5 text-4xl lg:text-6xl text-center md:text-left sm:text-5xl font-bold">Hi, I am  <span className="text-purple-600">Sumit</span></h1>
-        <h2  className="mt-3 flex flex-col text-center text-4xl font-bold md:text-left sm:text-5xl">Frontent React Developer</h2>
+        <h2  className="mt-3 flex flex-col text-center text-4xl font-bold md:text-left sm:text-5xl">Frontend React Developer</h2>
         <p className="mt-5 text-center text-xl text-gray-600 md:text-left">I'm a frondend react developer and this is my official portfolio website to showes all details and work exprins web development.</p>
         <div className="mt-7 flex justify-center md:justify-start">
           <a className="inline-block rounded-md bg-purple-500 drop-shadow-lg hover:drop-shadow-2xl px-5 py-2 font-medium text-white" href="https://drive.google.com/file/d/1-0YEP1QIx4y37K7UeBd-bvtmbwhktBVb/view?usp=sharing" target="_blank">download Resume</a>
