e275c794370ebe1fbd0b7eae743ed98c3801381f
sumit saini <sumitsaini95488@gmail.com>
2022-09-16 13:58:14 +0530

    add our resume

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -12,7 +12,7 @@ function Home() {
         <p class="mt-5 flex flex-col text-center text-3xl font-bold sm:text-left sm:text-4xl md:text-5xl">Hi, I am <span className="text-purple-500">Sumit,</span> <span>Frontent React Developer</span></p>
         <p class="mt-5 text-center text-xl text-gray-600 sm:text-left">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
         <div class="mt-7 flex justify-center sm:justify-start">
-          <a class="inline-block rounded-md bg-purple-500 px-5 py-2 font-medium text-white" href="https://media.discordapp.net/attachments/937339895591079957/1006444456918126592/Olivia_Wilson.png?width=298&height=421" target="_blank">download Resume</a>
+          <a class="inline-block rounded-md bg-purple-500 px-5 py-2 font-medium text-white" href="https://drive.google.com/file/d/1-0YEP1QIx4y37K7UeBd-bvtmbwhktBVb/view?usp=sharing" target="_blank">download Resume</a>
         </div>
       </div>
     </div>
